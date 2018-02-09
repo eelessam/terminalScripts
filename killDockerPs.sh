ef0bdf96b253e1792e874b784b06a18abb8fdba0
Sam Eeles <sam.eeles@gamesys.co.uk>
2018-02-09 11:53:21 +0000

    Clean branches script

--- killDockerPs.sh
+++ killDockerPs.sh
@@ -1,4 +1,4 @@
 PROCESS_NAME=$1
 
-docker ps | grep $PROCESS_NAME | awk '{ print  }' | xargs -I {} docker rm -f {}
+docker ps | grep $PROCESS_NAME | awk '{ print $1 }' | xargs -I {} docker rm -f {}
 
